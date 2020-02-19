d4e40dda8fc599529d0611b5a875f17294f9228f
westcsc <west_b@sbcglobal.net>
2020-02-19 16:40:49 -0600

    add expensify styles

--- src/components/ExpenseList.js
+++ src/components/ExpenseList.js
@@ -5,16 +5,29 @@ import selectExpenses from '../selectors/expenses';
 
 export const ExpenseList = (props) => {
     return (
-        <div>
-            {props.expenses.map((expense) => 
-                <ExpenseListItem key={expense.id} {...expense} />)}
+        <div className="content-container">
+            <div className="list-header">
+                <div className="show-for-mobile">Expenses</div>
+                <div className="show-for-desktop">Expense</div>
+                <div className="show-for-desktop">Amount</div>
+            </div>
+            <div className="list-body">
+                {
+                    props.expenses.length === 0 ?(
+                        <div className="list-item list-item--message">
+                            <span>No Expenses</span>
+                        </div>
+                ) : (
+                props.expenses.map((expense) => 
+                    <ExpenseListItem key={expense.id} {...expense} />)
+                )}
+            </div>
         </div>
-)};
-
-const mapStateToProps = (state) => {
-    return {
-        expenses: selectExpenses(state.expenses, state.filters)
-    };
+    )
 };
+
+const mapStateToProps = (state) => ({
+        expenses: selectExpenses(state.expenses, state.filters)
+});
 
 export default connect(mapStateToProps)(ExpenseList);
